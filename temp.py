532433b1d8823da6481f8a46c2da8cd7bff9835f
jnhurtado <jnhurtado@uc.cl>
2021-01-11 16:06:54 -0300

    cambios

--- temp.py
+++ temp.py
@@ -2,20 +2,19 @@
 """
 Código predictor de oleaje
 """
-<<<<<<< HEAD
+
 
 import numpy as np
 import random
 import sys
 import IPython
-import tensorflow as tf
+import tensorflow
 from tensorflow import keras
 from keras.callbacks import ModelCheckpoint
 from keras.models import Model, load_model, Sequential
 from keras.layers import Dense, Activation, Dropout, Input, Masking, TimeDistributed, LSTM, Conv1D
 from keras.layers import GRU, Bidirectional, BatchNormalization, Reshape
 from keras.optimizers import Adam
-=======
-print("hola mundo")
-print("hola de nuevo mundo")
->>>>>>> 3f28dca6ac0da232b427b0ed800a95faac64293b
+
+
+
